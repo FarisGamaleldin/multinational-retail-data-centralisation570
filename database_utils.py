e73f64a56c436d4ece3dc95550bf3f317d4fe84d
Faris Gamaleldin <farisfoads@gmail.com>
2025-01-21 19:29:12 +0000

    Initialize project with DataExtractor, DatabaseConnector, and DataCleaning classes

--- database_utils.py
+++ database_utils.py
@@ -4,26 +4,14 @@ import pandas as pd
 
 class DatabaseConnector:
     def __init__(self, db_config_path='db_creds.yaml'):
-        """
-        Initialize the DatabaseConnector with a path to the credentials file.
-        :param db_config_path: Path to the YAML file containing database credentials.
-        """
         self.db_config_path = db_config_path
 
     def read_db_creds(self):
-        """
-        Read database credentials from a YAML file.
-        :return: Dictionary containing database credentials.
-        """
         with open(self.db_config_path, 'r') as file:
             creds = yaml.safe_load(file)
         return creds
 
     def init_db_engine(self):
-        """
-        Initialize and return a SQLAlchemy database engine.
-        :return: SQLAlchemy engine.
-        """
         creds = self.read_db_creds()
         engine = create_engine(
             f"postgresql://{creds['RDS_USER']}:{creds['RDS_PASSWORD']}@{creds['RDS_HOST']}:{creds['RDS_PORT']}/{creds['RDS_DATABASE']}"
@@ -31,10 +19,6 @@ class DatabaseConnector:
         return engine
 
     def list_db_tables(self):
-        """
-        List all tables in the database.
-        :return: List of table names.
-        """
         engine = self.init_db_engine()
         with engine.connect() as connection:
             result = connection.execute(
@@ -44,11 +28,6 @@ class DatabaseConnector:
         return tables
 
     def upload_to_db(self, df, table_name):
-        """
-        Upload a Pandas DataFrame to a specified table in the database.
-        :param df: DataFrame to upload.
-        :param table_name: Name of the table to upload data to.
-        """
         try:
             # Initialize the database engine
             engine = self.init_db_engine()
